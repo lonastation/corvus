5de43055d645ac8b5417c5154805dec3c76bf8f7
lanitaleon <1912164282@qq.com>
2023-10-01 20:42:15 +0800

    stock: search text field

--- lib/stock_tab.dart
+++ lib/stock_tab.dart
@@ -10,17 +10,36 @@ class StockTab extends StatefulWidget {
     return _StockTabState();
   }
 }
-class _StockTabState extends State<StockTab> {
 
+class _StockTabState extends State<StockTab> {
   @override
   void initState() {
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Everything is here'),
+        toolbarHeight: 80,
+        title: const TextField(
+          decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+                borderRadius: BorderRadius.all(Radius.circular(14.0)),
+              ),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(14.0))),
+              hintText: 'Enter a search term',
+              hintStyle: TextStyle(
+                color: Colors.lightBlue,
+                fontWeight: FontWeight.normal,
+                fontSize: 26,
+              ),
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 10, horizontal: 20)),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -31,24 +50,46 @@ class _StockTabState extends State<StockTab> {
               ));
         },
         foregroundColor: Colors.blue,
-        child: const Icon(
-          Icons.add
-        ),
+        child: const Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       body: ListView(
-        children: const <Widget>[
+        children: <Widget>[
           ListTile(
-            leading: Icon(Icons.map),
-            title: Text('Map'),
+            title: const Text(
+              '小豆泥玩偶',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            trailing: IconButton(
+              color: Colors.blueAccent,
+              onPressed: () {},
+              icon: const Icon(Icons.create_outlined),
+            ),
+            subtitle: const Text('床上'),
           ),
           ListTile(
-            leading: Icon(Icons.photo_album),
-            title: Text('Album'),
+            title: const Text(
+              '北海巨妖耳机',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            trailing: IconButton(
+              color: Colors.blueAccent,
+              onPressed: () {},
+              icon: const Icon(Icons.create_outlined),
+            ),
+            subtitle: const Text('桌上'),
           ),
           ListTile(
-            leading: Icon(Icons.phone),
-            title: Text('Phone'),
+            title: const Text(
+              '棉签',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            trailing: IconButton(
+              color: Colors.blueAccent,
+              onPressed: () {},
+              icon: const Icon(Icons.create_outlined),
+            ),
+            subtitle: const Text('桌上'),
           ),
         ],
       ),
